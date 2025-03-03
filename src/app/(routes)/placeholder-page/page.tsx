ebb12068467b6d6bec746292fab83cb164dd4efa
donbr <dwbranson@gmail.com>
2025-03-02 21:47:50 -0800

    update linting config

--- src/app/(routes)/placeholder-page/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import React from 'react'
-import PlaceholderPage from '../../../components/pages/PlaceholderPage'
-
-export default function Page() {
-  return <PlaceholderPage />
-}
